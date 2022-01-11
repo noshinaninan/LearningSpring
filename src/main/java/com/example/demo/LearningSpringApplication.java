fb546055ca96445589002fdccd4edb1faf54c147
Noshina Ninan <noshzann@gmail.com>
2022-01-11 11:36:37 -0500

    added Room.java and RoomRepository.java and some change in Application class

---
+++ src/main/java/com/example/demo/LearningSpringApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.data.entity.Room;
+import com.example.demo.data.repository.RoomRepository;
+
+@SpringBootApplication
+public class LearningSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringApplication.class, args);
+	}
+	 @RestController
+	    @RequestMapping("/rooms")
+	    public class RoomController{
+	        @Autowired
+	        private RoomRepository roomRepository;
+
+	        @GetMapping
+	        public Iterable<Room> getRooms(){
+	            return this.roomRepository.findAll();
+	        }
+
+	    }
+}

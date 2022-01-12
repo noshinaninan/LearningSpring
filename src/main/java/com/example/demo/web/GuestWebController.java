9cc09718596f1299287f751590fc9728e191ca1c
Noshina Ninan <noshzann@gmail.com>
2022-01-12 11:26:57 -0500

    changes till 4th chapter

---
+++ src/main/java/com/example/demo/web/GuestWebController.java
@@ -0,0 +1,31 @@
+package com.example.demo.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.business.service.ReservationService;
+import com.example.demo.data.entity.Guest;
+
+@Controller
+@RequestMapping("/guests")
+public class GuestWebController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public GuestWebController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public String getGuests(Model model){
+        List<Guest> guests = this.reservationService.getHotelGuests();
+        model.addAttribute("guests", guests);
+        return "guests";
+    }
+}
